c0fd270608424ed56bbdac253a548dcff859eeb2
Alex Pacheco <alex.pacheco.s@gmail.com>
2017-10-15 15:06:54 -0300

    Beta ready

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'pages#home'
+  get 'send' => 'send#index'
+  post 'send' => 'send#create'
+  get '*unmatched_route', :to => 'application#render_404'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
